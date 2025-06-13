de1e9a214789968aa2b63e1e9655bd8acaabe896
Cashik777 <dmitrijvelikij921@gmail.com>
2025-06-13 18:27:40 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function spinWheel() {
-  const result = document.getElementById('result');
-  const prizes = ["$5", "$50", "$0", "$10", "$250", "$1000", "Try Again"];
-  const outcome = prizes[Math.floor(Math.random() * prizes.length)];
-  result.innerText = "🎉 You won: " + outcome + "!";
-}
